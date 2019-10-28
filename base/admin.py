58c02c8e036ca207b063959c53527ae6c18e9050
elias <eljo1391@gmail.com>
2019-10-28 19:42:16 -0300

    se cambia el idioma y se agrega al admin de django

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from base.models import Proyecto
+from base.models import Proyecto, LineaBase, Item
 
 # Register your models here.
 
 
 admin.site.register(Proyecto)
+admin.site.register(LineaBase)
+admin.site.register(Item)
